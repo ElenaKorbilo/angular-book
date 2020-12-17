2fc8faf03b1f45a63530388a5602c4fb8deb3bd0
ElenaKorbilo <olena.korbilo@gmail.com>
2020-12-17 20:49:51 +0200

    1 file modified

--- src/app/pipes/FilterPipe.ts
+++ src/app/pipes/FilterPipe.ts
@@ -6,6 +6,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class FilterPipe implements PipeTransform {
   transform(array: any, str: string, start?: any, end?: any): any {
+    if (str === "") return array;
     return array.filter(item => {
       return (
         item.title.includes(str) ||
